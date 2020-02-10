09e639f96b3346288899a000929d7896a1bb6853
Nora Liu <tzuyuanliu0830@gmail.com>
2020-02-11 01:40:06 +0800

    modify contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,25 @@
 pragma solidity 0.5.16;
 
 contract Election {
-  string public candidate;
+  // Model a Candidate
+  struct Candidate {
+    uint Candidate;
+    string name;
+    uint voteCount;
+  }
+  // Store Candidate
+  // Fetch Candidate
+  mapping(uint => Candidate) public candidates;
+  // Store Cndidate Count
+  uint public candidatesCount;
 
   constructor () public {
-    candidate = "Candidate 1";
+    addCandidate("Candidate 1");
+    addCandidate("Candidate 2");
+  }
+
+  function addCandidate (string memory _name) private {
+    candidatesCount ++;
+    candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
   }
 }
